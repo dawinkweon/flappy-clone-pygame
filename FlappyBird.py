9d38668cc40d736e85f760aa84ef130ef2bda0dc
presdk <windarena@gmail.com>
2020-08-18 18:28:54 +1200

    refactor(code style): prefix private members and functions with underscores to follow python standards

--- FlappyBird.py
+++ FlappyBird.py
@@ -3,29 +3,33 @@ from pygame.sprite import Sprite
 from Events import Events
 
 class FlappyBird(Sprite):
-    def __init__(self, start_pos, image, game_configuration):
+    def __init__(self, start_pos, image):
         Sprite.__init__(self)
         self.image = image
         self.rect = self.image.get_rect()
         self.rect.x = start_pos[0]
         self.rect.y = start_pos[1]
-        self.game_configuration = game_configuration
         self.velocity = 0
         self.acceleration = 1
         self.can_flap = True
         self.can_move = True
-    
+
+    def __do_accelerate__(self):
+        self.velocity = self.velocity + 0.5 * self.acceleration
+
+    def __move__(self):
+        self.rect.y += self.velocity
+
+    def kill(self):
+        self.velocity = 10
+        self.acceleration = 0
+        self.can_flap = False
+
     def do_flap(self):
         if self.can_flap:
             self.velocity = -8
 
-    def do_accelerate(self):
-        self.velocity = self.velocity + 0.5 * self.acceleration
-
-    def move(self):
-        self.rect.y += self.velocity
-
     def update(self):
         if self.can_move:
-            self.do_accelerate()
-            self.move()
+            self.__do_accelerate__()
+            self.__move__()
--- PipeContainer.py
+++ PipeContainer.py
@@ -8,42 +8,44 @@ from GameConfiguration import GameConfiguration
 class PipeContainer:
     # Each pipe has a top and bottom part
     NUM_PIPE_PARTS = 2 
+    
     def __init__(self, asset_factory, game_configuration):
-        self.asset_factory = asset_factory
-        self.game_configuration = game_configuration
+        self._asset_factory = asset_factory
+        self._game_configuration = game_configuration
+
         self.pipe_group = Group()
         self.can_move = True
 
-        self.game_configuration.min_pipe_height = int(self.game_configuration.window_height * 0.1)
-        self.max_num_pipes = int(self.game_configuration.window_width / (self.game_configuration.pipe_width + self.game_configuration.pipe_distance)) * PipeContainer.NUM_PIPE_PARTS + 2 # Add 2 for some leeway
+        self._min_pipe_height = int(self._game_configuration.window_height * 0.1)
+        self._max_num_pipes = int(self._game_configuration.window_width / (self._game_configuration.pipe_width + self._game_configuration.pipe_distance)) * PipeContainer.NUM_PIPE_PARTS + 2 # Add 2 for some leeway
         
-        self.initialize_pipes()
+        self.__initialize_pipes__()
 
-    def initialize_pipes(self):
-        first_pipe_index = int(self.game_configuration.pipe_start_pos / (self.game_configuration.pipe_width + self.game_configuration.pipe_distance))
+    def __initialize_pipes__(self):
+        first_pipe_index = int(self._game_configuration.pipe_start_pos / (self._game_configuration.pipe_width + self._game_configuration.pipe_distance))
 
-        for i in range(first_pipe_index, self.max_num_pipes):
-            pipe_x = i * (self.game_configuration.pipe_width + self.game_configuration.pipe_distance)
-            self.create_pipe(pipe_x)
+        for i in range(first_pipe_index, self._max_num_pipes):
+            pipe_x = i * (self._game_configuration.pipe_width + self._game_configuration.pipe_distance)
+            self.__create_pipe__(pipe_x)
 
-    def create_pipe(self, pipe_x):
-        random_pos = random.randint(self.game_configuration.min_pipe_height, self.game_configuration.window_height - self.game_configuration.min_pipe_height - self.game_configuration.pipe_gap_height)
-        top_height = self.game_configuration.window_height - (random_pos + self.game_configuration.pipe_gap_height)
+    def __create_pipe__(self, pipe_x):
+        random_pos = random.randint(self._min_pipe_height, self._game_configuration.window_height - self._min_pipe_height - self._game_configuration.pipe_gap_height)
+        top_height = self._game_configuration.window_height - (random_pos + self._game_configuration.pipe_gap_height)
         
-        top_pipe = self.create_pipe_part(pipe_x, 0, self.game_configuration.pipe_width, top_height, PipeDirection.Down)
-        bottom_pipe = self.create_pipe_part(pipe_x, self.game_configuration.window_height - random_pos, self.game_configuration.pipe_width, random_pos, PipeDirection.Up)
+        top_pipe = self.__create_pipe_part__(pipe_x, 0, self._game_configuration.pipe_width, top_height, PipeDirection.Down)
+        bottom_pipe = self.__create_pipe_part__(pipe_x, self._game_configuration.window_height - random_pos, self._game_configuration.pipe_width, random_pos, PipeDirection.Up)
 
         self.pipe_group.add(top_pipe)
         self.pipe_group.add(bottom_pipe)
         
-    def create_pipe_part(self, x, y, width, height, pipe_direction):
-        pipe_img = self.asset_factory.create_pipe_image((width, height), pipe_direction)
-        return Pipe(x, y, pipe_img, self.game_configuration.map_move_speed)
+    def __create_pipe_part__(self, x, y, width, height, pipe_direction):
+        pipe_img = self._asset_factory.create_pipe_image((width, height), pipe_direction)
+        return Pipe(x, y, pipe_img, self._game_configuration.map_move_speed)
 
-    def add_pipe_from_end(self):
+    def __add_pipe_from_end__(self):
         last_pipe = self.pipe_group.sprites()[-1]
-        new_pipe_x = last_pipe.rect.x + self.game_configuration.pipe_width + self.game_configuration.pipe_distance
-        self.create_pipe(new_pipe_x)
+        new_pipe_x = last_pipe.rect.x + self._game_configuration.pipe_width + self._game_configuration.pipe_distance
+        self.__create_pipe__(new_pipe_x)
 
     def update(self, surface):
         if self.can_move:
@@ -52,7 +54,7 @@ class PipeContainer:
         
         # Add more pipes if there's space
         # Each pipe actually has two parts, top and bottom
-        num_sprites_to_add = self.max_num_pipes - len(self.pipe_group.sprites())
+        num_sprites_to_add = self._max_num_pipes - len(self.pipe_group.sprites())
         if (num_sprites_to_add > 0):
             for i in range(num_sprites_to_add):
-                self.add_pipe_from_end()
+                self.__add_pipe_from_end__()
